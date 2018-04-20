eaa343204c3e47f077180938391b298e314eda43
Giovani <sousa_giovani@hotmail.com>
2018-04-20 18:12:43 -0300

    Simplify views

--- quotes_challenge/quotes_challenge/views.py
+++ quotes_challenge/quotes_challenge/views.py
@@ -1,4 +1,11 @@
+from pyramid.view import view_config
+from pyramid.response import Response
 from pyramid.view import notfound_view_config
+
+
+@view_config(route_name='home')
+def home_view(request):
+    return Response('<h1>Desafio Web 1.0</h1>')
 
 
 @notfound_view_config(renderer='../templates/404.jinja2')
--- quotes_challenge/quotes_challenge/views/quotes.py
+++
@@ -1,7 +0,0 @@
-from pyramid.view import view_config
-from pyramid.response import Response
-
-
-@view_config(route_name='home')
-def home_view(request):
-    return Response('<h1>Desafio Web 1.0</h1>')
