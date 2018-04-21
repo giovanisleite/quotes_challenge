e3ef60c51b6bb8dd03bc7eb271172dcfda3aa187
Giovani <sousa_giovani@hotmail.com>
2018-04-21 00:46:51 -0300

    Add a view to show all quotes in bullet points

--- quotes_challenge/tests/test_views.py
+++ quotes_challenge/tests/test_views.py
@@ -1,7 +1,16 @@
 from pyramid.testing import DummyRequest
-from ..views import home_view
+from unittest.mock import patch
+
+from ..views import home_view, quotes_view
 
 
-def test_home():
+def test_home_view():
     response = home_view(DummyRequest())
     assert(response.status_int == 200)
+
+
+@patch('quotes_challenge.views.get_quotes')
+def test_quotes_view(get_quotes):
+    get_quotes.return_value = {'quotes': 'python -m this'.split()}
+    response = quotes_view(DummyRequest())
+    assert('quotes' in response)
--- quotes_challenge/views.py
+++ quotes_challenge/views.py
@@ -2,13 +2,20 @@ from pyramid.view import view_config
 from pyramid.response import Response
 from pyramid.view import notfound_view_config
 
+from .quotes import get_quotes
+
 
 @view_config(route_name='home')
 def home_view(request):
     return Response('<h1>Desafio Web 1.0</h1>')
 
 
-@notfound_view_config(renderer='../templates/404.jinja2')
+@view_config(route_name='quotes', renderer='templates/quotes.jinja2')
+def quotes_view(request):
+    return get_quotes()
+
+
+@notfound_view_config(renderer='templates/404.jinja2')
 def notfound_view(request):
     request.response.status = 404
     return {}
